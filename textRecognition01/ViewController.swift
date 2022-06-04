4acdadfdd5dfd72cb05ab1d3e4b598ea565fa1a4
yelerty <yelerty@gmail.com>
2022-06-04 16:19:14 +0900

    Initial Commit

---
+++ textRecognition01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textRecognition01
+//
+//  Created by jihong on 6/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
